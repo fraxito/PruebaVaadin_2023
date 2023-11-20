5c08dbfea694a141b91f95603a143fa7547cecf4
Jorge Cisneros <fraxito@gmail.com>
2023-11-20 17:08:20 +0100

    Lunes 20-Nov

--- src/main/java/org/dis/front/MyUI.java
+++ src/main/java/org/dis/front/MyUI.java
@@ -6,11 +6,13 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.dis.back.BRException;
+import org.dis.back.EmpleadoBR;
+import org.dis.back.TipoEmpleado;
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -22,20 +24,58 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class MyUI extends UI {
 
+    private TextField creaLabel(String texto){
+        TextField etiqueta = new TextField();
+        etiqueta.setCaption(texto);
+        return etiqueta;
+    }
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Pon aquí tu nombre:");
+        final HorizontalLayout salarioBruto = new HorizontalLayout();
+        final HorizontalLayout salarioNeto = new HorizontalLayout();
+        final VerticalLayout salarioBrutoContenedor = new VerticalLayout();
+        final VerticalLayout salarioNetoContenedor = new VerticalLayout();
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        //TextField tipo = creaLabel("Tipo de empleado");
+        ComboBox <String> tipoEmpleadoComboBox = new ComboBox<>("Tipo de empleado",
+                Arrays.asList(TipoEmpleado.ENCARGADO, TipoEmpleado.VENDEDOR));
+
+        TextField ventasMes = creaLabel("Ventas del mes");
+        TextField horasExtra = creaLabel("Horas extra");
+
+        TextField inputSalarioBruto = creaLabel("Salario bruto del empleado");
+
+        Button botonSalarioBruto = new Button("Calcula Salario Bruto");
+        botonSalarioBruto.addClickListener(e -> {
+            String tipoEmpleadoIn = tipoEmpleadoComboBox.getValue();
+            double ventasMesIn = Double.parseDouble(ventasMes.getValue());
+            double horasExtraIn = Double.parseDouble(horasExtra.getValue());
+            EmpleadoBR empleado = new EmpleadoBR();
+            try {
+                double resultado = empleado.calculaSalarioBruto(tipoEmpleadoIn, ventasMesIn, horasExtraIn);
+                Label labelSalarioBruto = new Label("El salario bruto del empleado es: " + resultado + " €");
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            } catch (BRException ex) {
+                Label labelSalarioBruto = new Label("ERROR: " + ex.getMessage() );
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            }
         });
-        
-        layout.addComponents(name, button);
+        Button botonSalarioNeto = new Button("Calcula Salario Neto");
+        botonSalarioNeto.addClickListener(e -> {
+
+        });
+
+        salarioNeto.addComponent(inputSalarioBruto);
+        salarioBruto.addComponents(tipoEmpleadoComboBox, ventasMes, horasExtra);
+        salarioBrutoContenedor.addComponents(salarioBruto, botonSalarioBruto);
+        salarioNetoContenedor.addComponents(salarioNeto, botonSalarioNeto);
+        TabSheet tabsheet = new TabSheet();
+        tabsheet.addTab(salarioBrutoContenedor).setCaption("Calcula Salario Bruto");
+        tabsheet.addTab(salarioNetoContenedor).setCaption("Calcula Salario Neto");
+
+        layout.addComponent(tabsheet);
         
         setContent(layout);
     }
